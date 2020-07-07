78e6d002c65a2a1b226134d3728374c4bbb9efca
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-07-07 06:48:26 +0200

    Flexbox markup for the Result pages

--- sections/res.php
+++ sections/res.php
@@ -1,218 +1,166 @@
 <?php
 
-	if (isset($alpha_resultSize) == true) {
+	echo'
+		<h3 class="hidden">Result sections</h3>
+	';
+	if (isset($alpha_resultSize)) {
 		$currentLimit = 0;
 		$pages = ceil($alpha_resultSize / $defaultListGrid);
+		$n = 0;
 		for ($i = 0; $i < $pages; $i++) {
+			$notAtStart = $i !== 0;
+			$notAtEnd = $i != $pages - 1;//TODO: bug that makes the strict comparison not work with pages
+			$limit = 0;
 			echo'
-				<section id="res' . $i . '" class="sections" data-sitemap="List of Results - p. ' . ($i+1) . '">
-					<h3 class="hidden">
-						Result section
-					</h3>
+				<section id="res' . $i . '" class="sections" data-sitemap="List of Results - p. ' . ($i + 1) . '">
 					<div class="container">
-						<div class="row col-12-xs">
-							<ul>
+						<header>
+							<ul class="containerRow">
+								<li class="previousPage' . ($notAtStart ? '' : ' disabled') . '">
+									<a' . ($notAtStart ? (' href="#res' . ($i - 1) . '"') : '') . ' title="Previous page, please!">
+										<span>Prev</span>
+									</a>
+								</li><!-- previousPage ends -->
+								<li class="nextPage' . ($notAtEnd ? '' : ' disabled') . '">
+									<a' . ($notAtEnd ? (' href="#res' . ($i + 1) . '"') : '') . ' title="Next page, please!">
+										<span>Next</span>
+									</a>
+								</li><!-- nextPage ends -->
+							</ul><!-- containerRow ends -->
+						</header><!-- header ends -->
+						<div class="containerColumn">
+							<div class="containerRow">
+								<h4>
+									<a href="#qua" title="Return to the Qualification page!">List of Results</a>
+								</h4>
+							</div><!-- containerRow ends -->
 			';
-			if ($i != 0) {
+			for ($j = $n; $j < $alpha_resultSize && $defaultListGrid !== $limit; $j++) {
+				$emptyLink = is_null($alpha_result[$j]->getWebsite());
+				$anchorClass = $emptyLink ? 'disabled' : '';
+				$hrefTag = $emptyLink ? '' : 'href="' . $alpha_result[$j]->getWebsite() . '"';
+				$startDate = date_format(new DateTime($alpha_result[$j]->getBeginning()), 'M jS, Y');
+				$endDate = $alpha_result[$j]->getEnding() === NULL ? 'UFN' : date_format(new DateTime($alpha_result[$j]->getEnding()), 'M jS, Y');
 				echo'
-								<li class="previousPage">
-									<a href="#res' . ($i-1) . '" title="Previous page, please!">
-										<span>
-											Prev
-										</span>
-									</a>
-								</li>
+							<div class="containerColumn force-left">
+								<div class="containerSwap force-left">
+									<div class="containerRow force-text-right">
+										<span class="force-mini-left">Agent: </span>
+										<a class="' . $anchorClass . '" ' . $hrefTag . ' target="_blank" title="Visit the official website of the testing agent!">' . $alpha_result[$j]->getAgent() . '</a>
+									</div><!-- containerRow ends -->
+									<div class="containerRow">
+										<span class="force-mini-left">Test: </span>
+										<a class="viewTarget" href="#sco' . $j . '" title="Visit the page regarding the test results!">' . $alpha_result[$j]->getName() . '</a>
+									</div><!-- containerRow ends -->
+								</div><!-- containerSwap ends -->
+								<div class="containerSwap force-left">
+									<div class="containerRow force-text-right">
+										<span class="force-mini-left">Date: </span>
+										<p>' . $startDate . ' - ' . $endDate . '</p>
+									</div><!-- containerRow ends -->
+									<div class="containerRow">
+										<span class="force-mini-left">Score: </span>
+										<p>' . $alpha_result[$j]->getScore() . '</p>
+									</div><!-- containerRow ends -->
+								</div><!-- containerSwap ends -->
+							</div><!-- containerColumn ends -->
 				';
-			}
-			if ($i != $pages-1) {
-				echo'
-								<li class="nextPage">
-									<a href="#res' . ($i+1) . '" title="Next page, please!">
-										<span>
-											Next
-										</span>
-									</a>
-								</li>
-				';
+				$n = $j + 1;
+				$limit++;
 			}
 			echo'
-							</ul>
-						</div>
-						<div class="content row col-12-xs">
-							<h4>
-								<a href="#qua" title="Return to the Qualification page!">
-									List of Results
-								</a>
-							</h4>
-			';
-			for ($j = $currentLimit; $j < $alpha_resultSize; $j++) {
-				echo'
-							<div class="row col-12-xs">
-								<div class="col-5-sm col-10-xs col-xs-push-1 text-left force-text-right no-margin">
-				';
-				if (is_null($alpha_result[$j]->getWebsite())) {
-					echo'
-									<p>
-										<span class="force-mini-left">Agent: </span>' . $alpha_result[$j]->getAgent() . '
-									</p>
-					';
-				}
-				else {
-					echo'
-									<p>
-										<a href="' . $alpha_result[$j]->getWebsite() . '" target="_blank" title="Visit the official website of the testing agent!">
-											<span class="force-mini-left">Agent: </span>' . $alpha_result[$j]->getAgent() . '
-										</a>
-									</p>
-					';
-				}
-				echo'
-								</div>
-								<div class="col-6-sm col-10-xs col-xs-push-1 text-left no-margin">
-									<p>
-										<a class="viewTarget" href="#sco' . $j . '" title="Visit the page regarding the test results!">
-											<span class="force-mini-left">Test: </span>' . $alpha_result[$j]->getName() . '
-										</a>
-									</p>
-								</div>
-								<div class="col-5-sm col-10-xs col-xs-push-1 text-left force-text-right no-margin">
-									<p>
-										<span class="force-mini-left">Date: </span>' . ($alpha_result[$j]->getEnding() == NULL ? "UFN" : date_format(new DateTime($alpha_result[$j]->getEnding()), 'M jS, Y')) . '
-									</p>
-								</div>
-								<div class="col-6-sm col-10-xs col-xs-push-1 text-left no-margin">
-									<p>
-										<span class="force-mini-left">Score: </span>' . $alpha_result[$j]->getScore() . '
-									</p>
-								</div>
-							</div>
-				';
-				$currentLimit++;
-				if ($currentLimit % $defaultListGrid == 0) {
-					break;
-				}
-			}
-			echo'
-						</div>
+						</div><!-- containerColumn ends -->
 					</div><!-- container ends -->
 				</section><!-- section ends -->
 			';
 		}
 		for ($i = 0; $i < $alpha_resultSize; $i++) {
+			$notAtStart = $i !== 0;
+			$notAtEnd = $i !== $alpha_resultSize - 1;
+			$emptyLink = is_null($alpha_result[$i]->getWebsite());
+			$anchorClass = $emptyLink ? 'disabled' : '';
+			$hrefTag = $emptyLink ? '' : 'href="' . $alpha_result[$i]->getWebsite() . '"';
+			$startDate = date_format(new DateTime($alpha_result[$i]->getBeginning()), 'M jS, Y');
+			$endDate = $alpha_result[$i]->getEnding() === NULL ? 'UFN' : date_format(new DateTime($alpha_result[$i]->getEnding()), 'M jS, Y');
+			$date = $alpha_result[$i]->getBeginning() === $alpha_result[$i]->getEnding() ? $startDate : $startDate . ' - ' . $endDate;
 			echo'
 				<section id="sco' . $i . '" class="sections" data-sitemap="' . $alpha_result[$i]->getName() . '">
-					<h3 class="hidden">
-						Testimonial section
-					</h3>
 					<div class="container">
-						<div class="row col-12-xs">
-							<ul>
-			';
-			if ($i != 0) {
-				echo'
-								<li class="previousPage">
-									<a href="#sco' . ($i-1) . '" title="Previous page, please!">
-										<span>
-											Prev
-										</span>
+						<header>
+							<ul class="containerRow">
+								<li class="previousPage' . ($notAtStart ? '' : ' disabled') . '">
+									<a' . ($notAtStart ? (' href="#sco' . ($i - 1) . '"') : '') . ' title="Previous page, please!">
+										<span>Prev</span>
 									</a>
-								</li>
-				';
-			}
-			if ($i != $alpha_resultSize-1) {
-				echo'
-								<li class="nextPage">
-									<a href="#sco' . ($i+1) . '" title="Next page, please!">
-										<span>
-											Next
-										</span>
+								</li><!-- previousPage ends -->
+								<li class="nextPage' . ($notAtEnd ? '' : ' disabled') . '">
+									<a' . ($notAtEnd ? (' href="#sco' . ($i + 1) . '"') : '') . ' title="Next page, please!">
+										<span>Next</span>
 									</a>
-								</li>
-				';
-			}
-			echo'
-							</ul>
-						</div>
-						<div class="content row col-12-xs">
-							<h4>
-								<a href="#res0" title="Return to the Results page!">
-									' . $alpha_result[$i]->getScore() . ' at ' . $alpha_result[$i]->getName() . '
-								</a>
-							</h4>
-							<div class="row col-12-xs">
-								<div class="col-12-xs no-margin">
-									<div class="col-4-sm col-12-xs text-left force-text-right">
-			';
-			if (is_null($alpha_result[$i]->getWebsite())) {
-				echo'
-										<p>
-											<span class="force-mini-left">Agent: </span>' . $alpha_result[$i]->getAgent() . '
-										</p>
-				';
-			}
-			else {
-				echo'
-										<p>
-											<a href="' . $alpha_result[$i]->getWebsite() . '" target="_blank" title="Visit the official website!">
-												<span class="force-mini-left">Agent: </span>' . $alpha_result[$i]->getAgent() . '
-											</a>
-										</p>
-				';
-			}
-			echo'
-									</div>
-									<div class="col-6-sm col-12-xs text-left">
-										<p>
-											<span class="force-mini-left">Name: </span>' . $alpha_result[$i]->getName() . '
-										</p>
-									</div>
-								</div>
-								<div class="row col-12-xs no-margin">
-									<div class="col-4-sm col-12-xs text-left force-text-right">
-										<p>
-											<span class="force-mini-left">Date: </span>' . ($alpha_result[$i]->getBeginning() == $alpha_result[$i]->getEnding() ? date_format(new DateTime($alpha_result[$i]->getBeginning()), 'M jS, Y') : date_format(new DateTime($alpha_result[$i]->getBeginning()), 'M jS, Y') . ' - ' . ($alpha_result[$i]->getEnding() == NULL ? "UFN" : date_format(new DateTime($alpha_result[$i]->getEnding()), 'M jS, Y'))) . '
-										</p>
-									</div>
-									<div class="col-6-sm col-12-xs text-left">
-										<p>
-											<span class="force-mini-left">Score: </span>' . $alpha_result[$i]->getScore() . '
-										</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-7-sm col-12-xs col-sm-push-4 no-padding">
-								<div class="row col-12-xs text-justify">
-									<p>
-										' . $alpha_result[$i]->getDescription() . '
-									</p>
-								</div>
-							</div>
-						</div>
+								</li><!-- nextPage ends -->
+							</ul><!-- containerRow ends -->
+						</header><!-- header ends -->
+						<div class="containerColumn">
+							<div class="containerRow">
+								<h4>
+									<a href="#res0" title="Return to the Results page!">' . $alpha_result[$i]->getScore() . ' at ' . $alpha_result[$i]->getName() . '</a>
+								</h4>
+							</div><!-- containerRow ends -->
+							<div class="containerColumn force-left">
+								<div class="containerSwap force-left">
+									<div class="containerRow force-text-right">
+										<span class="force-mini-left">Agent: </span>
+										<p>' . $alpha_result[$i]->getAgent() . '</p>
+									</div><!-- containerRow ends -->
+									<div class="containerRow">
+										<span class="force-mini-left">Name: </span>
+										<p>' . $alpha_result[$i]->getName() . '</p>
+									</div><!-- containerRow ends -->
+								</div><!-- containerSwap ends -->
+								<div class="containerSwap force-left">
+									<div class="containerRow force-text-right">
+										<span class="force-mini-left">Date: </span>
+										<p>' . $date . '</p>
+									</div><!-- containerRow ends -->
+									<div class="containerRow">
+										<span class="force-mini-left">Score: </span>
+										<p>' . $alpha_result[$i]->getScore() . '</p>
+									</div><!-- containerRow ends -->
+								</div><!-- containerSwap ends -->
+							</div><!-- containerColumn ends -->
+							<div class="containerSwap">
+								<div class="containerColumn">
+									<div class="containerColumn">
+										<p class="text-justify">' . $alpha_result[$i]->getDescription() . '</p>
+									</div><!-- containerColumn ends -->
+									<div class="containerColumn">
+									</div><!-- containerColumn ends -->
+								</div><!-- containerColumn ends -->
+								<div class="containerColumn">
+									<div class="containerColumn">
+									</div><!-- containerColumn ends -->
+									<div class="containerColumn">
+									</div><!-- containerColumn ends -->
+								</div><!-- containerColumn ends -->
+							</div><!-- containerSwap ends -->
+						</div><!-- containerColumn ends -->
 					</div><!-- container ends -->
 				</section><!-- section ends -->
 			';
 		}
-	}
-	else {
+	} else {
 		echo'
 			<section id="res0" class="sections" data-sitemap="Empty | Error">
-				<h3 class="hidden">
-					Result section
-				</h3>
 				<div class="container">
-					<div class="content row col-12-xs">
-						<h4>
-							Unable to load data / no data to be loaded - regarding the Result pages
-						</h4>
-						<div class="row col-12-xs">
-							<strong>
-								Currently, this page was unable to load! Please, try again later!
-							</strong>
-						</div>
-					</div>
+					<div class="containerColumn">
+						<h4>Unable to load data / no data to be loaded - regarding the Result pages</h4>
+						<div class="containerRow">
+							<strong>Currently, this page was unable to load! Please, try again later!</strong>
+						</div><!-- containerRow ends -->
+					</div><!-- containerColumn ends -->
 				</div><!-- container ends -->
 			</section><!-- section ends -->
 		';
 	}
-	
+
 ?>
