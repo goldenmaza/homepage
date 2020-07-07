db181f15193e9613c8b91126df9cf7e1e3498e27
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-07-07 12:20:10 +0200

    Flexbox markup for the Testimonial pages

--- sections/tes.php
+++ sections/tes.php
@@ -1,218 +1,162 @@
 <?php
 
-	if (isset($alpha_testimonialSize) == true) {
+	echo'
+		<h3 class="hidden">Testimonial sections</h3>
+	';
+	if (isset($alpha_testimonialSize)) {
 		$currentLimit = 0;
 		$pages = ceil($alpha_testimonialSize / $defaultListGrid);
+		$n = 0;
 		for ($i = 0; $i < $pages; $i++) {
+			$notAtStart = $i !== 0;
+			$notAtEnd = $i != $pages - 1;//TODO: bug that makes the strict comparison not work with pages
+			$limit = 0;
 			echo'
-				<section id="tes' . $i . '" class="sections" data-sitemap="List of Testimonials - p. ' . ($i+1) . '">
-					<h3 class="hidden">
-						Testimonial section
-					</h3>
+				<section id="tes' . $i . '" class="sections" data-sitemap="List of Testimonials - p. ' . ($i + 1) . '">
 					<div class="container">
-						<div class="row col-12-xs">
-							<ul>
+						<header>
+							<ul class="containerRow">
+								<li class="previousPage' . ($notAtStart ? '' : ' disabled') . '">
+									<a' . ($notAtStart ? (' href="#tes' . ($i - 1) . '"') : '') . ' title="Previous page, please!">
+										<span>Prev</span>
+									</a>
+								</li><!-- previousPage ends -->
+								<li class="nextPage' . ($notAtEnd ? '' : ' disabled') . '">
+									<a' . ($notAtEnd ? (' href="#tes' . ($i + 1) . '"') : '') . ' title="Next page, please!">
+										<span>Next</span>
+									</a>
+								</li><!-- nextPage ends -->
+							</ul><!-- containerRow ends -->
+						</header><!-- header ends -->
+						<div class="containerColumn">
+							<div class="containerRow">
+								<h4>
+									<a href="#qua" title="Return to the Qualification page!">List of Testimonials</a>
+								</h4>
+							</div><!-- containerRow ends -->
 			';
-			if ($i != 0) {
+			for ($j = $n; $j < $alpha_testimonialSize && $defaultListGrid !== $limit; $j++) {
+				$emptyLink = is_null($alpha_testimonial[$j]->getWebsite());
+				$anchorClass = $emptyLink ? 'disabled' : '';
+				$hrefTag = $emptyLink ? '' : 'href="' . $alpha_testimonial[$j]->getWebsite() . '"';
+				$date = date_format(new DateTime($alpha_testimonial[$j]->getAuthored()), 'M jS, Y');
 				echo'
-								<li class="previousPage">
-									<a href="#tes' . ($i-1) . '" title="Previous page, please!">
-										<span>
-											Prev
-										</span>
-									</a>
-								</li>
+							<div class="containerColumn force-left">
+								<div class="containerSwap force-left">
+									<div class="containerRow force-text-right">
+										<span class="force-mini-left">Employer: </span>
+										<a class="' . $anchorClass . '" ' . $hrefTag . ' target="_blank" title="Visit the official company website!">' . substr($alpha_testimonial[$j]->getCompany(), 0, strpos($alpha_testimonial[$j]->getCompany(), ' ')) . '</a>
+									</div><!-- containerRow ends -->
+									<div class="containerRow">
+										<span class="force-mini-left">Author: </span>
+										<a class="viewTarget" href="#quo' . $j . '" title="Visit the page regarding the testimonial given by ' . $alpha_testimonial[$j]->getAuthor() . '!">' . substr($alpha_testimonial[$j]->getAuthor(), 0, strpos($alpha_testimonial[$j]->getAuthor(), ',')) . '</a>
+									</div><!-- containerRow ends -->
+								</div><!-- containerSwap ends -->
+								<div class="containerSwap force-left">
+									<div class="containerRow force-text-right">
+										<span class="force-mini-left">Date: </span>
+										<p>' . $date . '</p>
+									</div><!-- containerRow ends -->
+									<div class="containerRow">
+										<span class="force-mini-left">Summary: </span>
+										<p>' . $alpha_testimonial[$j]->getShort() . '</p>
+									</div><!-- containerRow ends -->
+								</div><!-- containerSwap ends -->
+							</div><!-- containerColumn ends -->
 				';
-			}
-			if ($i != $alpha_testimonialSize-1) {
-				echo'
-								<li class="nextPage">
-									<a href="#tes' . ($i+1) . '" title="Next page, please!">
-										<span>
-											Next
-										</span>
-									</a>
-								</li>
-				';
+				$n = $j + 1;
+				$limit++;
 			}
 			echo'
-							</ul>
-						</div>
-						<div class="content row col-12-xs">
-							<h4>
-								<a href="#qua" title="Return to the Qualification page!">
-									List of Testimonials
-								</a>
-							</h4>
-			';
-			for ($j = $currentLimit; $j < $alpha_testimonialSize; $j++) {
-				echo'
-							<div class="row col-12-xs">
-								<div class="col-5-sm col-12-xs text-left force-text-right no-margin">
-				';
-				if (is_null($alpha_testimonial[$j]->getWebsite())) {
-					echo'
-									<p>
-										<span class="force-mini-left">Employer: </span>' . substr($alpha_testimonial[$j]->getCompany(), 0, strpos($alpha_testimonial[$j]->getCompany(), ' ')) . '
-									</p>
-					';
-				}
-				else {
-					echo'
-									<p>
-										<a href="' . $alpha_testimonial[$j]->getWebsite() . '" target="_blank" title="Visit the official company website!">
-											<span class="force-mini-left">Employer: </span>' . substr($alpha_testimonial[$j]->getCompany(), 0, strpos($alpha_testimonial[$j]->getCompany(), ' ')) . '
-										</a>
-									</p>
-					';
-				}
-				echo'
-								</div>
-								<div class="col-6-sm col-12-xs text-left no-margin">
-									<p>
-										<a class="viewTarget" href="#quo' . $j . '" title="Visit the page regarding the testimonial given by ' . $alpha_testimonial[$j]->getAuthor() . '!">
-											<span class="force-mini-left">Author: </span>' . substr($alpha_testimonial[$j]->getAuthor(), 0, strpos($alpha_testimonial[$j]->getAuthor(), ',')) . '
-										</a>
-									</p>
-								</div>
-								<div class="col-5-sm col-12-xs text-left force-text-right no-margin">
-									<p>
-										<span class="force-mini-left">Date: </span>' . date_format(new DateTime($alpha_testimonial[$j]->getAuthored()), 'M jS, Y') . '
-									</p>
-								</div>
-								<div class="col-6-sm col-12-xs text-left no-margin">
-									<p>
-										<span class="force-mini-left">Summary: </span><q> ' . $alpha_testimonial[$j]->getShort() . ' </q>
-									</p>
-								</div>
-							</div>
-				';
-				$currentLimit++;
-				if ($currentLimit % $defaultListGrid == 0) {
-					break;
-				}
-			}
-			echo'
-						</div>
+						</div><!-- containerColumn ends -->
 					</div><!-- container ends -->
 				</section><!-- section ends -->
 			';
 		}
 		for ($i = 0; $i < $alpha_testimonialSize; $i++) {
+			$notAtStart = $i !== 0;
+			$notAtEnd = $i !== $alpha_testimonialSize - 1;
+			$emptyLink = is_null($alpha_testimonial[$i]->getWebsite());
+			$anchorClass = $emptyLink ? 'disabled' : '';
+			$hrefTag = $emptyLink ? '' : 'href="' . $alpha_testimonial[$i]->getWebsite() . '"';
+			$date = date_format(new DateTime($alpha_testimonial[$i]->getAuthored()), 'M jS, Y');
 			echo'
 				<section id="quo' . $i . '" class="sections" data-sitemap="' . $alpha_testimonial[$i]->getAuthor() . '">
-					<h3 class="hidden">
-						Testimonial section
-					</h3>
 					<div class="container">
-						<div class="row col-12-xs">
-							<ul>
-			';
-			if ($i != 0) {
-				echo'
-								<li class="previousPage">
-									<a href="#quo' . ($i-1) . '" title="Previous page, please!">
-										<span>
-											Prev
-										</span>
+						<header>
+							<ul class="containerRow">
+								<li class="previousPage' . ($notAtStart ? '' : ' disabled') . '">
+									<a' . ($notAtStart ? (' href="#quo' . ($i - 1) . '"') : '') . ' title="Previous page, please!">
+										<span>Prev</span>
 									</a>
-								</li>
-				';
-			}
-			if ($i != $alpha_testimonialSize-1) {
-				echo'
-								<li class="nextPage">
-									<a href="#quo' . ($i+1) . '" title="Next page, please!">
-										<span>
-											Next
-										</span>
+								</li><!-- previousPage ends -->
+								<li class="nextPage' . ($notAtEnd ? '' : ' disabled') . '">
+									<a' . ($notAtEnd ? (' href="#quo' . ($i + 1) . '"') : '') . ' title="Next page, please!">
+										<span>Next</span>
 									</a>
-								</li>
-				';
-			}
-			echo'
-							</ul>
-						</div>
-						<div class="content row col-12-xs">
-							<h4>
-								<a href="#tes0" title="Return to the Testimonials page!">
-									Testimonial by ' . $alpha_testimonial[$i]->getAuthor() . '
-								</a>
-							</h4>
-							<div class="row col-12-xs">
-								<div class="col-12-xs no-margin">
-									<div class="col-4-sm col-12-xs text-left force-text-right">
-			';
-			if (is_null($alpha_testimonial[$i]->getWebsite())) {
-				echo'
-										<p>
-											<span class="force-mini-left">Employer: </span>' . $alpha_testimonial[$i]->getCompany() . '
-										</p>
-				';
-			}
-			else {
-				echo'
-										<p>
-											<a href="' . $alpha_testimonial[$i]->getWebsite() . '" target="_blank" title="Visit the official company website!">
-												<span class="force-mini-left">Employer: </span>' . $alpha_testimonial[$i]->getCompany() . '
-											</a>
-										</p>
-				';
-			}
-			echo'
-									</div>
-									<div class="col-6-sm col-12-xs text-left">
-										<p>
-											<span class="force-mini-left">Author: </span>' . $alpha_testimonial[$i]->getAuthor() . '
-										</p>
-									</div>
-								</div>
-								<div class="row col-12-xs no-margin">
-									<div class="col-4-sm col-12-xs text-left force-text-right">
-										<p>
-											<span class="force-mini-left">Date: </span>' . date_format(new DateTime($alpha_testimonial[$i]->getAuthored()), 'M jS, Y') . '
-										</p>
-									</div>
-									<div class="col-6-sm col-12-xs no-padding">
-										<div class="col-12-xs text-justify">
-											<p>
-												<q> ' . $alpha_testimonial[$i]->getDescription() . ' </q> 
-												<br/><br/>
-												Note: This testimonial was translated by Richard and the original, can be
-												given on request. Some changes have been made to make it compact and minor
-												grammar changes	might have been made since a direct word-by-word translation
-												would not make much sense.
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+								</li><!-- nextPage ends -->
+							</ul><!-- containerRow ends -->
+						</header><!-- header ends -->
+						<div class="containerColumn">
+							<div class="containerRow">
+								<h4>
+									<a href="#tes0" title="Return to the Testimonials page!">Testimonial by ' . $alpha_testimonial[$i]->getAuthor() . '</a>
+								</h4>
+							</div><!-- containerRow ends -->
+							<div class="containerColumn force-left">
+								<div class="containerSwap force-left">
+									<div class="containerRow force-text-right">
+										<span class="force-mini-left">Employer: </span>
+										<a class="' . $anchorClass . '" ' . $hrefTag . ' target="_blank" title="Visit the official company website!">' . $alpha_testimonial[$i]->getCompany() . '</a>
+									</div><!-- containerRow ends -->
+									<div class="containerRow">
+										<span class="force-mini-left">Author: </span>
+										<p>' . $alpha_testimonial[$i]->getAuthor() . '</p>
+									</div><!-- containerRow ends -->
+								</div><!-- containerSwap ends -->
+								<div class="containerSwap force-left">
+									<div class="containerRow force-text-right">
+										<span class="force-mini-left">Date: </span>
+										<p>' . $date . '</p>
+									</div><!-- containerRow ends -->
+									<div class="containerRow force-text-right">
+									</div><!-- containerRow ends -->
+								</div><!-- containerSwap ends -->
+							</div><!-- containerColumn ends -->
+							<div class="containerSwap">
+								<div class="containerColumn">
+									<div class="containerColumn">
+										<q class="text-justify">' . htmlspecialchars_decode($alpha_testimonial[$i]->getDescription()) . '</q>
+										<p class="text-justify">Note: This testimonial was translated by Richard and the original, can be given on request. Some changes have been made to make it compact and minor grammar changes might have been made since a direct word-by-word translation would not make much sense.</p>
+									</div><!-- containerColumn ends -->
+									<div class="containerColumn">
+									</div><!-- containerColumn ends -->
+								</div><!-- containerColumn ends -->
+								<div class="containerColumn">
+									<div class="containerColumn">
+									</div><!-- containerColumn ends -->
+									<div class="containerColumn">
+									</div><!-- containerColumn ends -->
+								</div><!-- containerColumn ends -->
+							</div><!-- containerSwap ends -->
+						</div><!-- containerColumn ends -->
 					</div><!-- container ends -->
 				</section><!-- section ends -->
 			';
 		}
-	}
-	else {
+	} else {
 		echo'
 			<section id="tes0" class="sections" data-sitemap="Empty | Error">
-				<h3 class="hidden">
-					Testimonial section
-				</h3>
 				<div class="container">
-					<div class="content row col-12-xs">
-						<h4>
-							Unable to load data / no data to be loaded - regarding the Testimonial pages
-						</h4>
-						<div class="row col-12-xs">
-							<strong>
-								Currently, this page was unable to load! Please, try again later!
-							</strong>
-						</div>
-					</div>
+					<div class="containerColumn">
+						<h4>Unable to load data / no data to be loaded - regarding the Testimonial pages</h4>
+						<div class="containerRow">
+							<strong>Currently, this page was unable to load! Please, try again later!</strong>
+						</div><!-- containerRow ends -->
+					</div><!-- containerColumn ends -->
 				</div><!-- container ends -->
 			</section><!-- section ends -->
 		';
 	}
-	
+
 ?>
