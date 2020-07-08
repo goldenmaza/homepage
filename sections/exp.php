fb37f136bcfe8f798b504bb8978c8432e21349a4
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-07-08 11:13:14 +0200

    Flexbox markup for the Experience pages

--- sections/exp.php
+++ sections/exp.php
@@ -1,13 +1,13 @@
 <?php
 
-	if (isset($alpha_experienceSize) == true) {
-		$index = 0;
+	echo'
+		<h3 class="hidden">Experience sections</h3>
+	';
+	if (isset($alpha_experienceSize) && isset($alpha_informationSize)) {
 		$paragraphs = [];
-		if (isset($alpha_informationSize) == true) {
-			for ($i = 0; $i < $alpha_informationSize; $i++) {
-				if ($alpha_information[$i]->getPage() == "exp") {
-					$paragraphs[] = $alpha_information[$i]->getParagraph();
-				}
+		for ($i = 0; $i < $alpha_informationSize; $i++) {
+			if ($alpha_information[$i]->getPage() == 'exp') {
+				$paragraphs[] = $alpha_information[$i]->getParagraph();
 			}
 		}
 		$data = [];
@@ -16,57 +16,57 @@
 			$data[$alpha_experience[$i]->getCategory()][$alpha_experience[$i]->getValue()] = $alpha_experience[$i]->getGrading();
 			$category[$i] = $alpha_experience[$i]->getCategory();
 		}
-		$category = array_values(array_unique($category));
-		$categorySize = count($category);
+		$index = 0;
 		$paragraphsSize = count($paragraphs);
-		for ($i = 0; $i <= $categorySize; $i++) {
+		$category = array_values(array_unique($category));
+		$pages = count($category);
+		for ($i = 0; $i <= $pages; $i++) {
+			$sectionId = 'exp' . $i;
+			$heading = ($i > 0 ? $category[$i - 1] : "Summary");
+			$notAtStart = $i !== 0;
+			$notAtEnd = $i !== $pages;
 			echo'
-				<section id="exp' . $i . '" class="sections" data-sitemap="' . ($i > 0 ? $category[$i-1] : "Summary") . '">
-					<h3 class="hidden">
-						Experience section
-					</h3>
+				<section id="' . $sectionId . '" class="sections" data-sitemap="' . $heading . '">
 					<div class="container">
-						<div class="row col-12-xs">
-							<ul>
+						<header>
+							<ul class="containerRow">
+								<li class="previousPage' . ($notAtStart ? '' : ' disabled') . '">
+									<a' . ($notAtStart ? (' href="#exp' . ($i - 1) . '"') : '') . ' title="Previous page, please!">
+										<span>Prev</span>
+									</a>
+								</li><!-- previousPage ends -->
+								<li class="nextPage' . ($notAtEnd ? '' : ' disabled') . '">
+									<a' . ($notAtEnd ? (' href="#exp' . ($i + 1) . '"') : '') . ' title="Next page, please!">
+										<span>Next</span>
+									</a>
+								</li><!-- nextPage ends -->
+							</ul><!-- containerRow ends -->
+						</header><!-- header ends -->
+						<div class="containerColumn">
+							<div class="containerRow">
+								<h4>
+									<a href="#qua" title="Return to the Qualification page!">' . $heading . '</a>
+								</h4>
+							</div><!-- containerRow ends -->
+							<div class="containerSwap">
+								<div class="containerColumn">
 			';
-			if ($i != 0) {
+			if (isset($paragraphs)) {
+				if ($index === $paragraphsSize) {
+					$index = 1;
+				}
 				echo'
-								<li class="previousPage">
-									<a href="#exp' . ($i-1) . '" title="Previous page, please!">
-										<span>
-											Prev
-										</span>
-									</a>
-								</li>
-				';
-			}
-			if ($i != count($category)) {
-				echo'
-								<li class="nextPage">
-									<a href="#exp' . ($i+1) . '" title="Next page, please!">
-										<span>
-											Next
-										</span>
-									</a>
-								</li>
+									<p class="text-justify">' . htmlspecialchars_decode($paragraphs[$index++]) . '</p>
 				';
 			}
 			echo'
-							</ul>
-						</div>
-						<div class="content row col-12-xs">
-							<h4>
-								<a href="#qua" title="Return to the Qualification page!">
-									' . ($i > 0 ? $category[$i-1] : "Summary") . '
-								</a>
-							</h4>
-							<div class="row col-12-xs">
-								<div class="col-5-xl col-5-lg col-5-sm col-12-xs col-1-sm-offset col-0-xs-offset text-left">
-									<ul id="skill' . $i . '" class="skillTree">
+								</div><!-- containerColumn ends -->
+								<div class="containerColumn">
+									<ul id="skill' . $i . '" class="containerColumn skillTree">
 			';
 			if ($i > 0) {
 				foreach ($data as $cat => $arr) {
-					if ($category[$i-1] == $cat) {
+					if ($category[$i - 1] == $cat) {
 						$vals = NULL;
 						foreach ($arr as $value => $grading) {
 							$vals[$grading][] = $value;
@@ -74,98 +74,69 @@
 						foreach ($vals as $grading => $values) {
 							echo'
 										<li>
-											<p>
-												' . implode(", ", $values) . '
-											</p>
+											<p>' . implode(", ", $values) . '</p>
 										</li>
-										<li class="col-12-xs line">
-											<span class="expand ' . getLine($grading) . '"></span>
+										<li class="containerColumn line">
+											<span class="containerRow expand ' . getLine($grading) . '"></span>
 										</li>
 							';
 						}
 					}
 				}
-			}
-			else {
-				for ($j = 0; $j < $categorySize; $j++) {
-							echo'
+			} else {
+				for ($j = 0; $j < $pages; $j++) {
+					echo'
 										<li>
-											<p>
-												' . $category[$j] . '
-											</p>
+											<p>' . $category[$j] . '</p>
 										</li>
-										<li class="col-12-xs line">
-											<span class="expand ' . getLine(getAverage($data[$category[$j]])) . '"></span>
+										<li class="containerColumn line">
+											<span class="containerRow expand ' . getLine(getAverage($data[$category[$j]])) . '"></span>
 										</li>
-							';
+					';
 				}
 			}
 			echo'
-									</ul>
-								</div>
-								<div class="col-5-xl col-5-lg col-5-sm col-12-xs text-justify">
-			';
-			if (isset($alpha_informationSize) == true) {
-				if ($index == $paragraphsSize) {
-					$index = 1;
-				}
-				echo'
-									<p>' . $paragraphs[$index++] . '</p>
-				';
-			}
-			echo'
-								</div>
-							</div>
-						</div>
+									</ul><!-- skillTree ends -->
+								</div><!-- containerColumn ends -->
+							</div><!-- containerSwap ends -->
+						</div><!-- containerColumn ends -->
 					</div><!-- container ends -->
 				</section><!-- section ends -->
 			';
 		}	
-	}
-	else {
+	} else {
 		echo'
 			<section id="exp0" class="sections" data-sitemap="Empty | Error">
-				<h3 class="hidden">
-					Experience section
-				</h3>
 				<div class="container">
-					<div class="content row col-12-xs">
-						<h4>
-							Unable to load data / no data to be loaded - regarding the Experience pages
-						</h4>
-						<div class="row col-12-xs">
-							<strong>
-								Currently, this page was unable to load! Please, try again later!
-							</strong>
-						</div>
-					</div>
+					<div class="containerColumn">
+						<h4>Unable to load data / no data to be loaded - regarding the Experience pages</h4>
+						<div class="containerRow">
+							<strong>Currently, this page was unable to load! Please, try again later!</strong>
+						</div><!-- containerRow ends -->
+					</div><!-- containerColumn ends -->
 				</div><!-- container ends -->
 			</section><!-- section ends -->
 		';
 	}
 
 	function getAverage($array) {
-		$counter = array_sum($array);
-		$counter /= count($array);
-		$counter = round($counter);
-		return $counter;
+		return round(array_sum($array) / count($array));
 	}
 
 	function getLine($grading) {
 		$line = "beginnerLine";
+
 		if ($grading == 2) {
 			$line = "basicLine";
-		}
-		else if ($grading == 3) {
+		} else if ($grading == 3) {
 			$line = "goodLine";
-		}
-		else if ($grading == 4) {
+		} else if ($grading == 4) {
 			$line = "advanceLine";
-		}
-		else if ($grading == 5) {
+		} else if ($grading == 5) {
 			$line = "expertLine";
 		}
+
 		return $line;
 	}
-	
+
 ?>
