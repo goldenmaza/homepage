0ac0426efecc26c1a3b4b2b1d6d2cff1e793491a
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-07-08 23:35:30 +0200

    Flexbox markup for the Contact form

--- sections/sen.php
+++ sections/sen.php
@@ -1,204 +1,168 @@
 <?php
 
 	echo'
+		<h2 class="hidden">Contact pages</h2>
 		<section id="con" class="sections" data-sitemap="Contact form">
 			<div class="container contactContainer">
-				<div class="row col-12-xs">
-					<div class="row col-12-xs">
-						<h2>
-							Contact form
-						</h2>
-					</div>
-					<form enctype="application/x-www-form-urlencoded" method="get" action="#" onkeyup="validateForm()" onclick="validateForm()" >
-						<div class="row col-12-xs">
-							<div class="row col-8-sm col-12-xs col-sm-push-2">
+				<header>
+					<div class="containerRow">
+						<h3>Contact form</h3>
+					</div><!-- containerRow ends -->
+				</header><!-- header ends -->
+				<div class="containerColumn">
+					<form enctype="application/x-www-form-urlencoded" method="get" action="#" onkeyup="validateForm()" onclick="validateForm()">
+						<div class="containerSwap">
+							<div class="containerColumn exceptionColumn">
 								<label for="messageInput">
 									<!--[if lt IE 10]>
 									<span class="required">Message:</span>
 									<![endif]-->
-									<textarea id="messageInput" class="input" maxlength="2500" title="Please give a detailed message..." placeholder="Please give a detailed message..." onfocus="this.value=\'\'; this.onfocus=\'\';" accesskey="m" tabindex="11"></textarea>
-									<span id="textareaCounter"></span>
+									<textarea id="messageInput" class="input" maxlength="2500" title="Please give a detailed message..." placeholder="Please give a detailed message..." accesskey="m" tabindex="11" autocomplete="off"></textarea>
+									<span id="messageCounter"></span>
 								</label>
-							</div>
-							<div class="col-10-lg col-8-sm col-12-xs col-lg-push-1 col-sm-push-2">
-								<div class="row col-6-lg col-12-xs col-lg-push-3">
-									<div class="col-12-xs">
-										<label for="nameInput">
-											<!--[if lt IE 10]>
-											<span class="required">Name:</span>
-											<![endif]-->
-											<input id="nameInput" class="input" type="text" maxlength="50" title="What is your name?" placeholder="Name" accesskey="n" tabindex="12" />
-											<span id="nameCounter"></span>
-										</label>
-									</div>
-									<div class="col-12-xs">
-										<label for="mailInput">
-											<!--[if lt IE 10]>
-											<span class="required">E-mail:</span>
-											<![endif]-->
-											<input id="mailInput" class="input" type="text" maxlength="65" title="What is your e-mail?" placeholder="E-mail" accesskey="e" tabindex="13" />
-											<span id="mailCounter"></span>
-										</label>
-									</div>
-									<div class="col-12-xs">
-										<label for="topicInput">
-											<!--[if lt IE 10]>
-											<span class="required">Topic:</span>
-											<![endif]-->
-											<input id="topicInput" class="input" type="text" maxlength="50" title="What is the topic of this message?" placeholder="Topic" accesskey="t" tabindex="14" />
-											<span id="topicCounter"></span>
-										</label>
-									</div>
+							</div><!-- containerColumn ends -->
+							<div class="containerColumn exceptionColumn">
+								<label for="topicInput">
+									<!--[if lt IE 10]>
+									<span class="required">Topic:</span>
+									<![endif]-->
+									<input id="topicInput" class="input" type="text" maxlength="50" title="What is the topic of this message?" placeholder="Topic" accesskey="t" tabindex="14" autocomplete="off" />
+									<span id="topicCounter"></span>
+								</label>
+								<label for="nameInput">
+									<!--[if lt IE 10]>
+									<span class="required">Name:</span>
+									<![endif]-->
+									<input id="nameInput" class="input" type="text" maxlength="50" title="What is your name?" placeholder="Name" accesskey="n" tabindex="12" autocomplete="off" />
+									<span id="nameCounter"></span>
+								</label>
+								<label for="emailInput">
+									<!--[if lt IE 10]>
+									<span class="required">E-mail:</span>
+									<![endif]-->
+									<input id="emailInput" class="input" type="text" maxlength="65" title="What is your e-mail?" placeholder="E-mail" accesskey="e" tabindex="13" autocomplete="off" />
+									<span id="emailCounter"></span>
+								</label>
+							</div><!-- containerColumn ends -->
+						</div><!-- containerSwap ends -->
+						<div class="containerColumn">
+							<div class="containerSwap">
+								<label class="containerColumn exceptionColumn" for="clearButton">
+									<!--[if lt IE 10]>
+									<span>Clear button:</span>
+									<![endif]-->
+									<input id="clearButton" class="input" type="button" disabled="disabled" onclick="clearForm()" title="If you want to clear out this contact form, press here!" value="CLEAR!" accesskey="c" tabindex="15" />
+								</label>
+								<label class="containerColumn exceptionColumn" for="helpButton">
+									<!--[if lt IE 10]>
+									<span>Help button:</span>
+									<![endif]-->
+									<input id="helpButton" class="input" type="button" disabled="disabled" onclick="displayHelp()" title="If you need help with this contact form, press here!" value="HELP!" accesskey="h" tabindex="16" />
+								</label>
+								<label class="containerColumn exceptionColumn" for="sendButton">
+									<!--[if lt IE 10]>
+									<span>Send button:</span>
+									<![endif]-->
+									<input id="sendButton" class="input" type="button" disabled="disabled" onclick="storeData()" title="If you are ready to send this message, press here!" value="SEND!" accesskey="s" tabindex="17" />
+								</label>
+							</div><!-- containerColumn ends -->
+						</div><!-- containerColumn ends -->
+					</form><!-- form ends -->
+				</div><!-- containerColumn ends -->
+				<div class="containerColumn">
+					<div class="containerRow">
+						<strong>Please leave a detailed message, thank you!</strong>
+					</div><!-- containerRow ends -->
+				</div><!-- containerColumn ends -->
+				<div class="containerColumn">
+					<div id="formSummary" class="hidden">
+						<div class="containerColumn">
+							<div id="summaryMessage" class="containerSwap">
+								<div class="containerColumn exceptionColumn">
+									<strong></strong>
 								</div>
-								<div class="row col-8-lg col-12-xs col-lg-push-2">
-									<label for="clearButton">
-										<!--[if lt IE 10]>
-										<span>Clear button:</span>
-										<![endif]-->
-										<input id="clearButton" class="input" type="button" disabled="disabled" onclick="clearForm()" title="If you want to clear out this contact form, press here!" value="CLEAR!" accesskey="c" tabindex="15" />
-									</label>
-									<label for="helpButton">
-										<!--[if lt IE 10]>
-										<span>Help button:</span>
-										<![endif]-->
-										<input id="helpButton" class="input" type="button" disabled="disabled" onclick="displayHelp()" title="If you need help with this contact form, press here!" value="HELP!" accesskey="h" tabindex="16" />
-									</label>
-									<label for="sendButton">
-										<!--[if lt IE 10]>
-										<span>Send button:</span>
-										<![endif]-->
-										<input id="sendButton" class="input" type="button" disabled="disabled" onclick="storeData()" title="If you are ready to send this message, press here!" value="SEND!" accesskey="s" tabindex="17" />
-									</label>
+								<div class="containerColumn exceptionColumn">
+									<p></p>
 								</div>
 							</div>
-						</div>
-					</form>
-				</div>
-				<div class="row col-12-xs">
-					<strong>Please leave a detailed message, thank you!</strong>
-				</div>
-				<div class="row col-12-xs">
-					<div id="contactSummary" class="hidden">
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
+							<div id="summaryTopic" class="containerSwap">
+								<div class="containerColumn exceptionColumn">
+									<strong></strong>
+								</div>
+								<div class="containerColumn exceptionColumn">
+									<p></p>
+								</div>
 							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
+							<div id="summaryName" class="containerSwap">
+								<div class="containerColumn exceptionColumn">
+									<strong></strong>
+								</div>
+								<div class="containerColumn exceptionColumn">
+									<p></p>
+								</div>
 							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
+							<div id="summaryEmail" class="containerSwap">
+								<div class="containerColumn exceptionColumn">
+									<strong></strong>
+								</div>
+								<div class="containerColumn exceptionColumn">
+									<p></p>
+								</div>
 							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
+						</div><!-- containerColumn ends -->
+					</div><!-- formSummary ends -->
+					<div id="formHelp" class="hidden">
+						<div class="containerColumn">
+							<div id="helpMessage" class="containerSwap">
+								<div class="containerColumn exceptionColumn">
+									<strong></strong>
+								</div>
+								<div class="containerColumn exceptionColumn">
+									<p></p>
+								</div>
 							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
+							<div id="helpTopic" class="containerSwap">
+								<div class="containerColumn exceptionColumn">
+									<strong></strong>
+								</div>
+								<div class="containerColumn exceptionColumn">
+									<p></p>
+								</div>
 							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
+							<div id="helpName" class="containerSwap">
+								<div class="containerColumn exceptionColumn">
+									<strong></strong>
+								</div>
+								<div class="containerColumn exceptionColumn">
+									<p></p>
+								</div>
 							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
+							<div id="helpEmail" class="containerSwap">
+								<div class="containerColumn exceptionColumn">
+									<strong></strong>
+								</div>
+								<div class="containerColumn exceptionColumn">
+									<p></p>
+								</div>
 							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
-							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
-							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
-							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
-							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
-							</div>
-						</div>
-					</div>
-					<div id="contactHelp" class="hidden">
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
-							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
-							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
-							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
-							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
-							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
-							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
-							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
-							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
-							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
-							</div>
-						</div>
-						<div class="row col-11-md col-12-xs">
-							<div class="col-3-xl col-3-lg col-3-md col-3-sm col-3-xs text-right">
-								<p></p>
-							</div>
-							<div class="col-8-xl col-8-lg col-7-md col-7-sm col-7-xs col-0-lg-offset col-1-xs-offset text-left">
-								<p></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+						</div><!-- containerColumn ends -->
+					</div><!-- helpSection ends -->
+				</div><!-- containerColumn ends -->
+			</div><!-- container ends -->
+		</section><!-- section ends -->
 		<section id="sen" class="sections">
 			<div class="container">
-				<div class="content row col-12-xs">
-					<h2>
-						Message sent
-					</h2>
-					<div class="row col-12-xs">
-						<strong>
-							Thank you, for your message!
-						</strong>
-					</div>
-					<div class="row col-4-lg col-6-xs col-lg-push-4 col-xs-push-3 iconsBar">
-						<a class="row sent" href="#con" title="Return to the Contact form!">
-						</a>
-					</div>
-				</div>
-			</div>
-		</section>
+				<div class="containerColumn">
+					<h3>Message sent notification</h3>
+					<div class="containerRow">
+						<strong>Thank you, for your message!</strong>
+					</div><!-- containerRow ends -->
+					<div class="containerRow iconsBar">
+						<a class="row sent" href="#con" title="Return to the Contact form!"></a>
+					</div><!-- containerRow ends -->
+				</div><!-- containerColumn ends -->
+			</div><!-- container ends -->
+		</section><!-- section ends -->
 	';
 
 ?>
