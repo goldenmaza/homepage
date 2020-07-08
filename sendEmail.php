ca29d662568b4769eefc76f3cf7e5ec1d06c960b
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-07-08 23:52:17 +0200

    Configuration file has been added for the sendEmail.php script

--- sendEmail.php
+++ sendEmail.php
@@ -5,40 +5,42 @@
 	 *
 	 * This class is used for sending an e-mail.
 	 * 
-	 * @package				Homepage
-	 * @author				Richard M. Hellstrand
-	 * @copyright			Copyright (c) 2015-2017, Alpheria
-	 * @license				http://www.alpheria.com/about/license.html
-	 * @link				http://www.alpheria.com
-	 * @since				June 5th, 2017 - Version 1.0
+	 * @package      Homepage
+	 * @author       Mats Richard Hellstrand
+	 * @copyright    Copyright (c) 2015-2025, Alpheria
+	 * @license      TODO: http://
+	 * @link         TODO: http://
+	 * @since        July 8th, 2020 - Version 1.1
 	 */
-	 
 	// ===========================================================================
 
-	$formText 	 = 	  $_GET['formText'];
-	$formArray 	 = 	  explode(',', $formText);
-	$host 		 = 	  "public@hellstrand.org";
+	require_once("configuration.php");
 
-	if (!empty($formArray[0]) && !empty($formArray[1]) && !empty($formArray[2]) && !empty($formArray[3])) {
-		$name 		 = 	  $formArray[0];
-		$email 		 = 	  $formArray[1];
-		$subject 	 = 	  $name . " wanna talk about: " . $formArray[2];
-		$message 	 = 	  strip_tags($formArray[3], '');
+	$formText        =   $_GET['formText'];
+	$formArray       =   explode(',', $formText);
+	$host            =   $sendTo;
+	$crnl            =   '\r\n';
 
-		$headers 	 = 	  "";
-		$headers 	.= 	  "Date: " . date('D, j M Y H:i:s O') . "\r\n";
-		$headers 	.= 	  "From: " . $name . "<" . $email . ">\r\n";
-		$headers 	.= 	  "Return-Path: " . $email . "\r\n";
-		$headers 	.= 	  "Sender: " . $email . "\r\n";
-		$headers 	.= 	  "Reply-To: " . $email . "\r\n";
-		$headers 	.= 	  "Message-ID: " . md5(uniqid(time())) . "@" . $_SERVER['SERVER_NAME'] . "\r\n";
-		$headers 	.= 	  "X-Priority: 3" . "\r\n";
-		$headers 	.= 	  "X-Mailer: Alpheria's Custom PHP Mail Script" . "\r\n";
-		$headers 	.= 	  "MIME-Version: 1.0" . "\r\n";
-		$headers 	.= 	  "Content-Type: text/plain; charset=utf-8" . "\r\n";
-		$headers 	.= 	  "Content-Transfer-Encoding: 8bit" . "\r\n";
+	if (!empty($formArray)) {
+		$name        =  $formArray[2];
+		$email       =  $formArray[3];
+		$subject     =  $name . ' wanna talk about: ' . $formArray[1];
+		$message     =  strip_tags($formArray[0], '');
+
+		$headers     =  '';
+		$headers    .=  'Date: ' . date('D, j M Y H:i:s O') . $crnl;
+		$headers    .=  'From: ' . $name . '<' . $email . '>' . $crnl;
+		$headers    .=  'Return-Path: ' . $email . $crnl;
+		$headers    .=  'Sender: ' . $email . $crnl;
+		$headers    .=  'Reply-To: ' . $email . $crnl;
+		$headers    .=  'Message-ID: ' . md5(uniqid(time())) . '@' . $_SERVER['SERVER_NAME'] . $crnl;
+		$headers    .=  'X-Priority: 3' . $crnl;
+		$headers    .=  'X-Mailer: Goldenmaza\'s Custom PHP Mail Script' . $crnl;
+		$headers    .=  'MIME-Version: 1.0' . $crnl;
+		$headers    .=  'Content-Type: text/plain; charset=utf-8' . $crnl;
+		$headers    .=  'Content-Transfer-Encoding: 8bit' . $crnl;
 
 		mail($host, $subject, $message, $headers);
 	}
 
-?> 
+?>
