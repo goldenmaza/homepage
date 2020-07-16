e82b2b156920dc541e0f7493e0fc6028796577d7
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-07-16 11:29:12 +0200

    Refactoring of variable names for the Sitemap script

--- sitemap.php
+++ sitemap.php
@@ -11,16 +11,16 @@
 	 * @copyright			Copyright (c) 2015-2017, Mats Richard Hellstrand
 	 * @license				http://www.alpheria.com/about/license.html
 	 * @link				http://www.alpheria.com
-	 * @since				June 5th, 2017 - Version 1.0
+	 * @since				June 28th, 2020 - Version 1.1
 	 */
 
 	// ===========================================================================
 
 	$priorityArray = ["abo", "pro", "deg", "job", "rec", "quo"];
-	$updateArray = ["pro", "deg", "rec"];
+	$frequencyArray = ["pro", "deg", "rec"];
 	$bufferArray = explode(PHP_EOL, ob_get_contents());
 	$sectionArray = [];
-	$groupArray = [];
+	$attributeArray = [];
 	$finalArray = [[]];
 	$output = "";
 	$sitemapXML = "";
@@ -41,20 +41,20 @@
 	// Take out the attributes and their data...
 	foreach ($sectionArray as $sectionString) {
 		preg_match_all('~"(.*?)"~', $sectionString, $output);
-		$groupArray[] = $output[1];
+		$attributeArray[] = $output[1];
 	}
 
 	// Remove unwanted attributes...
-	for ($i = 0; $i < count($groupArray); $i++) {
-		foreach ($groupArray[$i] as $notUsed) {
-			unset($groupArray[$i][1]);
+	for ($i = 0; $i < count($attributeArray); $i++) {
+		foreach ($attributeArray[$i] as $notUsed) {
+			unset($attributeArray[$i][1]);
 		}
-		$groupArray[$i] = array_values($groupArray[$i]);
+		$attributeArray[$i] = array_values($attributeArray[$i]);
 	}
 
 	// Update the keys so they are now poiting to each type of pages instead of integers...
-	foreach ($groupArray as $group) {
-		$finalArray[preg_replace('/[0-9]+/', '', $group[0])][] = $group;
+	foreach ($attributeArray as $attribute) {
+		$finalArray[preg_replace('/[0-9]+/', '', $attribute[0])][] = $attribute;
 	}
 	unset($finalArray[0]);
 	array_pop($finalArray);
@@ -67,7 +67,7 @@
 			foreach ($pagesArray as $pagesKey => $pageArray) {
 			   $sitemapXML .= "\t<url>" . PHP_EOL;
 			   $sitemapXML .= "\t\t<loc>http://www.hellstrand.org/?#" . $pageArray[0] . "</loc>" . PHP_EOL;
-			   $sitemapXML .= "\t\t<changefreq>" . (in_array($finalKey, $updateArray) ? "monthly" : "yearly") . "</changefreq>" . PHP_EOL;
+			   $sitemapXML .= "\t\t<changefreq>" . (in_array($finalKey, $frequencyArray) ? "monthly" : "yearly") . "</changefreq>" . PHP_EOL;
 			   $sitemapXML .= "\t\t<priority>" . (in_array($finalKey, $priorityArray) ? "1.0" : "0.5") . "</priority>" . PHP_EOL;
 			   $sitemapXML .= "\t</url>" . PHP_EOL;
 			}
