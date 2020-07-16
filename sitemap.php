4cb1d18997505231b77a430b443eb8d7c27a4e4f
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-07-16 23:53:22 +0200

    More refactoring of the Sitemap script

--- sitemap.php
+++ sitemap.php
@@ -1,29 +1,28 @@
 <?php
 
 	/**
-	 * siemap.php
+	 * sitemap.php
 	 *
 	 * This file is used for generating the sitemap.xml file and to prepare
 	 * the sitemap data used for the map.php file.
-	 * 
-	 * @package				Homepage
-	 * @author				Richard M. Hellstrand
-	 * @copyright			Copyright (c) 2015-2017, Mats Richard Hellstrand
-	 * @license				http://www.alpheria.com/about/license.html
-	 * @link				http://www.alpheria.com
-	 * @since				June 28th, 2020 - Version 1.1
+	 *
+	 * @package     Homepage
+	 * @author      Mats Richard Hellstrand
+	 * @copyright   Copyright (c) 2015-2025, Mats Richard Hellstrand
+	 * @license     TODO: http://
+	 * @link        TODO: http://
+	 * @since       July 16th, 2020 - Version 1.2
 	 */
-
 	// ===========================================================================
 
-	$priorityArray = ["abo", "pro", "deg", "job", "rec", "quo"];
-	$frequencyArray = ["pro", "deg", "rec"];
+	$priorityArray = ['abo', 'pro', 'deg', 'job', 'rec', 'quo'];
+	$frequencyArray = ['pro', 'deg', 'rec'];
 	$bufferArray = explode(PHP_EOL, ob_get_contents());
 	$sectionArray = [];
 	$attributeArray = [];
-	$finalArray = [[]];
-	$output = "";
-	$sitemapXML = "";
+	$sitemapArray = [[]];
+	$output = '';
+	$sitemapXML = '';
 
 	// Trim/remove empty rows and rows with no html tags...
 	foreach ($bufferArray as &$row) {
@@ -33,7 +32,7 @@
 
 	// Find and save all section tags...
 	foreach ($bufferArray as $row) {
-		if (strpos($row, "<section id=") !== false) {
+		if (strpos($row, '<section id=') !== false) {
 			$sectionArray[] = $row;
 		}
 	}
@@ -54,25 +53,25 @@
 
 	// Update the keys so they are now poiting to each type of pages instead of integers...
 	foreach ($attributeArray as $attribute) {
-		$finalArray[preg_replace('/[0-9]+/', '', $attribute[0])][] = $attribute;
+		$sitemapArray[preg_replace('/[0-9]+/', '', $attribute[0])][] = $attribute;
 	}
-	unset($finalArray[0]);
-	array_pop($finalArray);
+	unset($sitemapArray[0]);
+	array_pop($sitemapArray);
 
-	// Construct XML data and save to the sitemap.xml file but only during once a month.
-	if (date("j") == "1") {
+	// Construct XML data and save to the sitemap.xml file, but only once a month.
+	if (date('j') === '1') {
 		$sitemapXML = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
 		$sitemapXML .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
-		foreach ($finalArray as $finalKey => $pagesArray) {
-			foreach ($pagesArray as $pagesKey => $pageArray) {
+		foreach ($sitemapArray as $sitemapKey => $sitemapArray) {
+			foreach ($sitemapArray as $pagesKey => $pageArray) {
 			   $sitemapXML .= "\t<url>" . PHP_EOL;
-			   $sitemapXML .= "\t\t<loc>http://www.hellstrand.org/?#" . $pageArray[0] . "</loc>" . PHP_EOL;
-			   $sitemapXML .= "\t\t<changefreq>" . (in_array($finalKey, $frequencyArray) ? "monthly" : "yearly") . "</changefreq>" . PHP_EOL;
-			   $sitemapXML .= "\t\t<priority>" . (in_array($finalKey, $priorityArray) ? "1.0" : "0.5") . "</priority>" . PHP_EOL;
+			   $sitemapXML .= "\t\t<loc>http://www.hellstrand.org/?#" . $pageArray[0] . '</loc>' . PHP_EOL;
+			   $sitemapXML .= "\t\t<changefreq>" . (in_array($sitemapKey, $frequencyArray) ? 'monthly' : 'yearly') . '</changefreq>' . PHP_EOL;
+			   $sitemapXML .= "\t\t<priority>" . (in_array($sitemapKey, $priorityArray) ? '1.0' : '0.5') . '</priority>' . PHP_EOL;
 			   $sitemapXML .= "\t</url>" . PHP_EOL;
 			}
 		}
-		$sitemapXML .= "</urlset>";
+		$sitemapXML .= '</urlset>';
 		file_put_contents('Sitemap.xml', $sitemapXML, LOCK_EX);
 	}
 
