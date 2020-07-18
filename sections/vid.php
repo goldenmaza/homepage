4f42f9fe14ccb400d0b17caa4da2d68489192e4c
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-07-18 14:02:02 +0200

    Flexbox markup for the Video page

--- sections/vid.php
+++ sections/vid.php
@@ -1,30 +1,26 @@
 <?php
 
 	echo'
-		<h1 class="hidden">
-			Videos
-		</h1>
+		<h2 class="hidden">Videos</h2>
 	';
 	for ($i = 0; $i < count($videoValues); $i++) {
 		echo'
 			<section id="vid0" class="sections">
 				<div class="container">
-					<div class="content row col-12-xs">
-						<div class="row col-12-xs">
-							<h2>
-								' . $videoValues[$i] . '
-							</h2>
-						</div>
-						<div class="row col-12-xs">
-							<object class="videoObject" type="application/x-shockwave-flash" height="100%" width="100%" data="http://www.youtube.com/v/lQ62_AoBY3Y?version=3">
+					<div class="containerColumn">
+						<div class="containerRow">
+							<h3>' . $videoValues[$i] . '</h3>
+						</div><!-- containerRow ends -->
+						<div class="containerColumn">
+							<object class="videoObject" type="application/x-shockwave-flash" height="100%" width="100%" data="http://www.youtube.com/v/###?version=3">
 								<param name="movie" value="http://www.youtube.com/v/lQ62_AoBY3Y?version=3"></param>
 								<param name="wmode" value="transparent"></param>
-							</object>
-						</div>
-					</div>
+							</object><!-- containerColumn ends -->
+						</div><!-- containerColumn ends -->
+					</div><!-- containerColumn ends -->
 				</div><!-- container ends -->
 			</section><!-- section ends -->
 		';
 	}
-	
+
 ?>
